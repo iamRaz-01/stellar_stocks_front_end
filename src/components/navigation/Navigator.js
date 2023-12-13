b08ad55cfaf1ac4f9f3cc1095ae2dc18d60250b5
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-13 17:39:27 +0530

    created logo

--- src/components/navigation/Navigator.js
+++ src/components/navigation/Navigator.js
@@ -1,45 +1,58 @@
 import "../../assets/css/universal.css"
 import "./navigator.css";
+import logo from "../../assets/images/stellarstocksLogo.png"
+import { useState } from "react";
 
 
+
+function click(){
+    
+}
 let navButtons = [
-    { name: "Create", icon: "control_point_duplicate" },
-    { name: "OverView", icon: "Dashboard" },
-    { name: "Bills", icon: "request_quote" },
-    { name: "Finance", icon: "currency_rupee" },
-    { name: "Me", icon: "badge" },
-    { name: "Settings", icon: "settings" },
-    { name: "Create Bill", icon: "list_alt_add" },
-    { name: "Add product", icon: "add_shopping_cart" }
+    { name: "Create", icon: "control_point_duplicate" , event : click },
+    { name: "OverView", icon: "Dashboard",event : click },
+    { name: "Bills", icon: "request_quote",event : click },
+    { name: "Finance", icon: "currency_rupee",event : click },
+    { name: "Me", icon: "badge",event : click },
+    { name: "Settings", icon: "settings",event : click }
 ]
-let create = [
-{ name: "Create Bill", icon: "list_alt_add" },
-{ name: "Add product", icon: "add_shopping_cart" }]
+// let create = [
+// { name: "Create Bill", icon: "list_alt_add" },
+// { name: "Add product", icon: "add_shopping_cart" }]
 
 function Navigator(){
+    let [sam,setSam] = useState(0);
+    const change  = ()=>{
+        setSam(sam+=1)
+    }
 
-    let buttons = navButtons.map(btn=> <NavButton name = {btn.name} icon = {btn.icon} />);
+
+
+    let buttons = navButtons.map(btn=> <NavButton name = {btn.name} icon = {btn.icon} event={change} />);
   
     return (
         <div className="navBar">
+            <img src={logo} alt="LOGO"  className="logo" />
             {buttons}
+            {sam}
+            
        
         </div>
     )
 
 }
-function CreateButton(){
-    let createBtns = create.map(c => <NavButton name = {c.name} icon = {c.icon} />);
-     return(
-        <div className="navDiv">
-           {createBtns} 
-        </div>
-     )
-}
+// function CreateButton(){
+//     let createBtns = create.map(c => <NavButton name = {c.name} icon = {c.icon} />);
+//      return(
+//         <div className="navDiv">
+//            {createBtns} 
+//         </div>
+//      )
+// }
 
 function NavButton(props){
     return(
-        <button className="navBtn">
+        <button className="navBtn" onClick={props.event}>
             <span className="material-symbols-outlined">
             {props.icon}
           </span>
