faae3d298c19e70c7a6cf29ba05a3f2ed0e2e4e3
Abdul razak <abdulrazak.nasriudeen@gmail.com>
2023-12-15 10:59:45 +0530

    updated Data on Sign up

--- src/components/sign/sign_up.js
+++ src/components/sign/sign_up.js
@@ -5,11 +5,12 @@ function SignIn(){
     return(
         <div className="signUpDiv">
             <h2>Sign Up     </h2>
-             <InputDiv icon="badge" />
-             <InputDiv icon="badge" />
-             <InputDiv icon="badge" />
-             <InputDiv icon="badge" />
-             <InputDiv icon="badge" />
+             <InputDiv icon="badge" placeholder="Enter Your Name" />
+             <InputDiv icon="call" placeholder="Enter Your Number" />
+             <InputDiv icon="mail" placeholder="Enter Your E-mail" />
+             <InputDiv icon="key" placeholder="Create Password " />
+             <InputDiv icon="key" placeholder="Confirm Password" />
+            
             
              <SubmitBtn />
         </div>
@@ -25,7 +26,7 @@ function InputDiv(prop){
             <span class="material-symbols-outlined">
  {prop.icon}
 </span>
-<input /> 
+<input  placeholder={prop.placeholder}/> 
 
 
         </div>
